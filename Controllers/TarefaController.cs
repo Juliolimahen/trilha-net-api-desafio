78af380b69f6896eb47a13e8277923f502845dd5
Juliolimahen <juliolima_henrique@outlook.com>
2023-09-21 22:10:49 -0300

    refatorando o codigo

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -10,51 +10,37 @@ namespace TrilhaApiDesafio.Controllers
     [Route("[controller]")]
     public class TarefaController : ControllerBase
     {
-        private readonly OrganizadorContext _context;
         private readonly ITarefaService _service;
 
         public TarefaController(OrganizadorContext context, ITarefaService service)
         {
-            _context = context;
             _service = service;
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> ObterPorId(int id)
         {
-            // TODO: Buscar o Id no banco utilizando o EF
-            // TODO: Validar o tipo de retorno. Se não encontrar a tarefa, retornar NotFound,
-            // caso contrário retornar OK com a tarefa encontrada
             var tarefa = await _service.GetByIdAsync(id);
-
-            if (tarefa is null)
-                return NotFound();
-
-            return Ok(tarefa);
+            return tarefa is null ? NotFound() : Ok(tarefa);
         }
 
         [HttpGet("ObterTodos")]
         public async Task<IActionResult> ObterTodos()
         {
-            // TODO: Buscar todas as tarefas no banco utilizando o EF
             var tarefas = await _service.GetAllAsync();
-            return Ok(tarefas);
+            return tarefas is null ? NotFound() : Ok(tarefas);
         }
 
         [HttpGet("ObterPorTitulo")]
         public async Task<IActionResult> ObterPorTitulo(string titulo)
         {
-            // TODO: Buscar  as tarefas no banco utilizando o EF, que contenha o titulo recebido por parâmetro
-            // Dica: Usar como exemplo o endpoint ObterPorData
-            // var tarefa = _context.Tarefas.Where(x => x.Titulo.Contains(titulo));
             var tarefa = await _service.ObterPorTitulo(titulo);
-            return Ok(tarefa);
+            return tarefa is null ? NotFound() : Ok(tarefa);
         }
 
         [HttpGet("ObterPorData")]
         public async Task<IActionResult> ObterPorData(DateTime data)
         {
-            // var tarefa = _context.Tarefas.Where(x => x.Data.Date == data.Date);
             var tarefa = await _service.ObterPorData(data);
             return Ok(tarefa);
         }
@@ -62,11 +48,8 @@ namespace TrilhaApiDesafio.Controllers
         [HttpGet("ObterPorStatus")]
         public async Task<IActionResult> ObterPorStatus(EnumStatusTarefa status)
         {
-            // TODO: Buscar  as tarefas no banco utilizando o EF, que contenha o status recebido por parâmetro
-            // Dica: Usar como exemplo o endpoint ObterPorData
-            // var tarefa = _context.Tarefas.Where(x => x.Status == status);
             var tarefa = await _service.ObterPorStatus(status);
-            return Ok(tarefa);
+            return tarefa is null ? NotFound() : Ok(tarefa);
         }
 
         [HttpPost]
@@ -92,8 +75,6 @@ namespace TrilhaApiDesafio.Controllers
             if (tarefa.Data == DateTime.MinValue)
                 return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
 
-            // TODO: Atualizar as informações da variável tarefaBanco com a tarefa recebida via parâmetro
-            // TODO: Atualizar a variável tarefaBanco no EF e salvar as mudanças (save changes)
             var tarefaInserida = await _service.UpdateAsync(tarefa);
             return Ok(tarefaInserida);
         }
@@ -101,13 +82,11 @@ namespace TrilhaApiDesafio.Controllers
         [HttpDelete("{id}")]
         public async Task<IActionResult> Deletar(int id)
         {
-            // var tarefaBanco = _context.Tarefas.Find(id);
             var tarefaBanco = await _service.GetByIdAsync(id);
 
             if (tarefaBanco == null)
                 return NotFound();
 
-            // TODO: Remover a tarefa encontrada através do EF e salvar as mudanças (save changes)
             await _service.DeleteAsync(tarefaBanco.Id);
             return NoContent();
         }
